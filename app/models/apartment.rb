e23374494e3388b694dfdcfb58b75df545d17967
learn <learn@learn09.local>
2016-09-19 16:46:02 -0700

    added map, dropdown for owner name, tested as well

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,3 +1,10 @@
 class Apartment < ActiveRecord::Base
   belongs_to :owner
+
+  validates :street, length: {minimum:1}
+  validates :postal_code, length: {minimum:5}
+  validates :state, length: {minimum:2}
+  validates :country, length: {minimum:1}
+
+
 end
--- spec/features/landing_pages_spec.rb
+++ spec/features/landing_pages_spec.rb
@@ -3,11 +3,38 @@ require 'rails_helper'
 RSpec.feature "LandingPages", type: :feature do
   context 'going to website' do
     Steps 'being welcomed' do
-      Given 'I am on the landing page' do
-        visit '/'
+      Given 'I am on the apartments page' do
+        visit '/apartments'
       end
       Then 'I can see a welcome message' do
         expect(page).to have_content("Listing")
+      end
+    end
+
+    Steps 'entering apartment' do
+      Given 'I have an owner called Alex' do
+        visit '/'
+        click_link 'New Owner'
+        fill_in 'owner_name', with: 'Alex'
+        click_button 'Create Owner'
+      end
+      And 'I am on apartments page' do
+        visit '/apartments'
+      end
+      Then 'I click on new apartment' do
+        expect(page).to have_content("New Apartment")
+        click_link 'New Apartment'
+      end
+      Then 'Fill in some forms' do
+        fill_in 'apartment_street', with: '123 Test'
+        fill_in 'apartment_postal_code', with: '12333'
+        fill_in 'apartment_state', with: 'CA'
+        fill_in 'apartment_country', with:'USA'
+        fill_in 'apartment_city', with: 'San Diego'
+      end
+      Then 'click the submit button' do
+        click_button 'Create Apartment'
+        expect(page).to have_content("Owner: Alex")
       end
     end
   end
