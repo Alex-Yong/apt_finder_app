c8beb30143544086c36574b36fa660dfe08db933
alex, adam <a.inho.yong@gmail.com>
2016-09-23 11:01:09 -0700

    seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :omniauthable,
   :recoverable, :rememberable, :trackable, :validatable
   has_many :apartments
+  
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
